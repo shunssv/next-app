215a7aa5944391354b646e4e61018fed200f97b2
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-02-27 18:11:06 -0800

    :sparkles: standardize useState between components

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -4,10 +4,33 @@ import { NextLogo } from '@/components/NextLogo';
 import { HeadDescription } from '@/components/HeadDescription';
 import { Header } from '@/components/Header';
 import Link from 'next/link';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useCounter } from '@/hooks/useCounter';
 import { useInputList } from '@/hooks/useInputList';
 
+const ITEMS = [
+  {
+    href: 'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Docs',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    href: 'https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Learn',
+    description: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    href: 'https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Templates',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy',
+    description:
+      'Instantly deploy your Next.js site to a shareable URL with Vercel.',
+  },
+];
 
 export function Main(props) {
   const handleClick = useCallback((e) => {
@@ -17,6 +40,14 @@ export function Main(props) {
 
   const {count, isShow, handleCounter, handleDisplay} = useCounter();
   const {text, array, handleInput, handleList} = useInputList();
+
+  const [items, setItems] = useState(ITEMS);
+  const handleReduce = useCallback(() => {
+    setItems((prevItems) => {
+      return prevItems.slice(0, prevItems.length - 1);
+    });
+  }, []);
+
 
   return (
     <main className={styles.main}>
@@ -44,7 +75,9 @@ export function Main(props) {
       <button onClick={handleList}>Add list</button>
 
       <NextLogo />
-      <Links />
+      
+      <p>{items.length}</p>
+      <Links items={items} handleReduce={handleReduce} />
     </main>
   );
 }
